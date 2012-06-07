40424f260bd07d914b90faedcfbcb743ff6fb7ef
Anh Tran <rilwis@gmail.com>
2012-06-07 08:20:29 +0700

    Reorganize files

--- pm4wp.php
+++ pm4wp.php
@@ -6,49 +6,54 @@ Description: Allow members of WordPress blog send and receive private messages (
 Version: 2.1.6
 Author: Rilwis
 Author URI: http://www.deluxeblogtips.com
-
-Copyright 2009-2010 Rilwis (email : rilwis@gmail.com)
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+License: GNU GPL 2+
 */
 
-include_once plugin_dir_path( __FILE__ ) . 'widget.php';
+// Prevent loading this file directly
+if ( !class_exists( 'WP' ) )
+{
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit;
+}
 
-// load text domain for plugin
-$locale = get_locale( );
-$mofile = WP_PLUGIN_DIR . '/' . plugin_basename( dirname( __FILE__ ) ) . '/lang/pm4wp' . '-' . $locale . '.mo';
-load_textdomain( 'pm4wp', $mofile );
+define( 'PM4WP_DIR', plugin_dir_path( __FILE__ ) );
+define( 'PM4WP_INC_DIR', trailingslashit( PM4WP_DIR . 'inc/' ) );
+
+define( 'PM4WP_URL', plugin_dir_url( __FILE__ ) );
+define( 'PM4WP_CSS_URL', trailingslashit( PM4WP_URL . 'css' ) );
+define( 'PM4WP_JS_URL', trailingslashit( PM4WP_URL . 'js' ) );
+
+include_once PM4WP_INC_DIR . 'widget.php';
+
+if ( is_admin() )
+{
+	include_once PM4WP_INC_DIR . 'options.php';
+}
+
+add_action( 'plugins_loaded', 'rwpm_load_text_domain' );
+register_activation_hook( __FILE__, 'rwpm_activate' );
 
 /**
- * Register an option group
+ * Load plugin text domain
+ *
+ * @return void
  */
-add_action( 'admin_init', 'rwpm_init' );
-
-function rwpm_init( ) {
-	register_setting( 'rwpm_option_group', 'rwpm_option' );
+function rwpm_load_text_domain()
+{
+	load_plugin_textdomain( 'pm4wp', trailingslashit( PM4WP_DIR . 'lang' ) );
 }
 
 /**
  * Create table and register an option when activate
+ *
+ * @return void
  */
-register_activation_hook( __FILE__, 'rwpm_activate' );
-
-function rwpm_activate( ) {
+function rwpm_activate()
+{
 	global $wpdb;
 
-	// create table
+	// Create table
 	$query = 'CREATE TABLE IF NOT EXISTS ' . $wpdb->prefix . 'pm (
 		`id` bigint(20) NOT NULL auto_increment,
 		`subject` text NOT NULL,
@@ -65,14 +70,14 @@ function rwpm_activate( ) {
 
 	$wpdb->query( $query );
 
-	// default numbers of PM for each group
+	// Default numbers of PM for each group
 	$default_option = array(
 		'administrator' => 0,
 		'editor' => 50,
 		'author' => 20,
 		'contributor' => 10,
 		'subscriber' => 5,
-		'type' => 'dropdown', // how to choose recipient: dropdown list or autocomplete based on user input
+		'type' => 'dropdown', // How to choose recipient: dropdown list or autocomplete based on user input
 		'email_enable' => 1,
 		'email_name' => '%BLOG_NAME%',
 		'email_address' => '%BLOG_ADDRESS%',
@@ -83,214 +88,9 @@ function rwpm_activate( ) {
 }
 
 /**
- * Delete table and option when uninstall
- */
-register_uninstall_hook( __FILE__, 'rwpm_uninstall' );
-
-function rwpm_uninstall( ) {
-	global $wpdb;
-
-	$wpdb->query( 'DROP table ' . $wpdb->prefix . 'pm' );
-
-	delete_option( 'rwpm_option' );
-}
-
-/**
- * Add Option page and PM Menu
- */
-add_action( 'admin_menu', 'rwpm_add_menu' );
-
-function rwpm_add_menu( ) {
-	global $wpdb, $current_user;
-
-	// get number of unread messages
-	$num_unread = $wpdb->get_var( 'SELECT COUNT(`id`) FROM ' . $wpdb->prefix . 'pm WHERE `recipient` = "' . $current_user->user_login . '" AND `read` = 0 AND `deleted` != "2"' );
-
-	if ( empty( $num_unread ) )
-		$num_unread = 0;
-
-	// option page
-	add_options_page( __( 'Private Messages Options', 'pm4wp' ), __( 'Private Messages', 'pm4wp' ), 'manage_options', 'rwpm_option', 'rwpm_option_page' );
-
-	// add Private Messages Menu
-	$icon_url = WP_PLUGIN_URL . '/' . plugin_basename( dirname( __FILE__ ) ) . '/icon.png';
-	add_menu_page( __( 'Private Messages', 'pm4wp' ), __( 'Messages', 'pm4wp' ) . "<span class='update-plugins count-$num_unread'><span class='plugin-count'>$num_unread</span></span>", 'read', 'rwpm_inbox', 'rwpm_inbox', $icon_url );
-
-	// inbox page
-	add_submenu_page( 'rwpm_inbox', __( 'Inbox', 'pm4wp' ), __( 'Inbox', 'pm4wp' ), 'read', 'rwpm_inbox', 'rwpm_inbox' );
-	// outbox page
-	add_submenu_page( 'rwpm_inbox', __( 'Outbox', 'pm4wp' ), __( 'Outbox', 'pm4wp' ), 'read', 'rwpm_outbox', 'rwpm_outbox' );
-
-	// send page	
-	$send_page = add_submenu_page( 'rwpm_inbox', __( 'Send Private Message', 'pm4wp' ), __( 'Send', 'pm4wp' ), 'read', 'rwpm_send', 'rwpm_send' );
-	add_action( "admin_print_styles-$send_page", 'rwpm_admin_print_styles' );
-}
-
-function rwpm_admin_print_styles( ) {
-	wp_enqueue_style( 'rwpm_css', plugins_url( 'css/style.css', __FILE__ ) );
-	wp_enqueue_script( 'rwpm_autosuggest_js', plugins_url( 'js/jquery.autoSuggest.packed.js', __FILE__ ), array( 'jquery' ) );
-	wp_enqueue_script( 'rwpm_js', plugins_url( 'js/script.js', __FILE__ ), array( 'rwpm_autosuggest_js' ) );
-}
-
-/**
- * Option page
- * Change number of PMs for each group
- */
-function rwpm_option_page( ) {
-	?>
-<div class="wrap">
-	<h2><?php _e( 'Private Messages Options', 'pm4wp' ); ?></h2>
-
-	<div style="width:600px;float:left">
-		<form method="post" action="options.php">
-
-			<?php
-   		settings_fields( 'rwpm_option_group' );
-			$option = get_option( 'rwpm_option' );
-
-			if ( empty( $option['hide_update'] ) ) {
-				echo '<div class="updated">',
-				'<p><strong>', __( '1. The plugin goes with a page template for front-end usage.', 'pm4wp' ), '</strong></p>',
-				'<p>', __( 'Copy file <code>pm4wp-template.php</code> to your theme folder and create a page with template <code>Private Messages</code>', 'pm4wp' ), '</p>',
-				'<p>', __( 'The template is just the backbone. You should modify it to fit your theme.', 'pm4wp' ), '</p>',
-				'<p></p><p><strong>', __( '2. You can send to multiple recipients now.', 'pm4wp' ), '</strong></p>',
-				'</div>';
-				echo '<input type="checkbox" name="rwpm_option[hide_update]"> ', __( 'Don\'t show this message next time', 'pm4wp' );
-			}
-
-			echo '<h3>', __( 'Please set numbers of private messages for each user role:', 'pm4wp' ), '</h3>';
-			echo '<p>', __( '<b><i>0</i></b> means <b><i>unlimited</i></b>', 'pm4wp' ), '</p>';
-			echo '<p>', __( '<b><i>-1</i></b> means <b><i>not allowed</i></b> to send PM', 'pm4wp' ), '</p>';
-
-
-			?>
-			<table class="form-table">
-				<tr>
-					<th><?php _e( 'Administrator', 'pm4wp' ); ?></th>
-					<td>
-						<input type="text" name="rwpm_option[administrator]" value="<?php echo $option['administrator']; ?>"/>
-					</td>
-				</tr>
-				<tr>
-					<th><?php _e( 'Editor', 'pm4wp' ); ?></th>
-					<td><input type="text" name="rwpm_option[editor]" value="<?php echo $option['editor']; ?>"/></td>
-				</tr>
-				<tr>
-					<th><?php _e( 'Author', 'pm4wp' ); ?></th>
-					<td><input type="text" name="rwpm_option[author]" value="<?php echo $option['author']; ?>"/></td>
-				</tr>
-				<tr>
-					<th><?php _e( 'Contributor', 'pm4wp' ); ?></th>
-					<td>
-						<input type="text" name="rwpm_option[contributor]" value="<?php echo $option['contributor']; ?>"/>
-					</td>
-				</tr>
-				<tr>
-					<th><?php _e( 'Subscriber', 'pm4wp' ); ?></th>
-					<td><input type="text" name="rwpm_option[subscriber]" value="<?php echo $option['subscriber']; ?>"/>
-					</td>
-				</tr>
-				<tr>
-					<th><?php _e( 'How do you want to choose recipient?', 'pm4wp' ); ?></th>
-					<td>
-						<input type="radio" name="rwpm_option[type]" value="dropdown" <?php if ( $option['type'] == 'dropdown' )
-							echo 'checked="checked"'; ?> /><?php _e( 'Dropdown list', 'pm4wp' ); ?>
-						<input type="radio" name="rwpm_option[type]" value="autosuggest" <?php if ( $option['type'] == 'autosuggest' )
-							echo 'checked="checked"'; ?> /><?php _e( 'Auto suggest from user input', 'pm4wp' ); ?>
-					</td>
-				</tr>
-			</table>
-
-			<h3><?php _e( 'Email template:', 'pm4wp' ); ?></h3>
-
-			<table class="form-table">
-				<tr>
-					<th><?php _e( 'Enable sending email when user receive new PM?', 'pm4wp' ); ?></th>
-					<td>
-						<input type="radio" name="rwpm_option[email_enable]" value="1" <?php if ( $option['email_enable'] )
-							echo 'checked="checked"'; ?> /> <?php _e( 'Yes', 'pm4wp' ); ?>
-						<input type="radio" name="rwpm_option[email_enable]" value="0" <?php if ( !$option['email_enable'] )
-							echo 'checked="checked"'; ?> /> <?php _e( 'No', 'pm4wp' ); ?>
-					</td>
-				</tr>
-				<tr>
-					<th><?php _e( 'From name (optional)', 'pm4wp' ); ?></th>
-					<td><input type="text" name="rwpm_option[email_name]" value="<?php echo $option['email_name']; ?>"/>
-					</td>
-				</tr>
-				<tr>
-					<th><?php _e( 'From email (optional)', 'pm4wp' ); ?></th>
-					<td>
-						<input type="text" name="rwpm_option[email_address]" value="<?php echo $option['email_address']; ?>"/>
-					</td>
-				</tr>
-				<tr>
-					<th><?php _e( 'Subject', 'pm4wp' ); ?></th>
-					<td>
-						<input type="text" name="rwpm_option[email_subject]" value="<?php echo $option['email_subject']; ?>"/>
-					</td>
-				</tr>
-				<tr>
-					<th><?php _e( 'Body', 'pm4wp' ); ?></th>
-					<td>
-						<textarea name="rwpm_option[email_body]" rows="10" cols="50"><?php echo $option['email_body']; ?></textarea><br/>
-						<?php _e( 'Allowed HTML tags: ', 'pm4wp' ); ?> a, br, b, i, u, img, ul, ol, li, hr
-					</td>
-				</tr>
-				<tr>
-					<th><strong><?php _e( 'Available tags', 'pm4wp' ); ?></strong></th>
-					<td>&nbsp;</td>
-				</tr>
-				<tr>
-					<th>%BLOG_NAME%</th>
-					<td><?php _e( 'Blog name', 'pm4wp' ) ?></td>
-				</tr>
-				<tr>
-					<th>%BLOG_ADDRESS%</th>
-					<td><?php _e( 'Email address of blog', 'pm4wp' ) ?></td>
-				</tr>
-				<tr>
-					<th>%SENDER%</th>
-					<td><?php _e( 'Sender name', 'pm4wp' ) ?></td>
-				</tr>
-				<tr>
-					<th>%INBOX_URL%</th>
-					<td><?php _e( 'URL of inbox', 'pm4wp' ) ?></td>
-				</tr>
-			</table>
-
-			<p class="submit">
-				<input type="submit" name="submit" class="button-primary" value="<?php _e( 'Save Changes', 'pm4wp' ) ?>"/>
-			</p>
-
-		</form>
-
-	</div>
-	<div style="width:200px;float:right;border:1px solid #ccc;padding:10px">
-		<h3><?php _e( 'Donation', 'pm4wp' ); ?></h3>
-
-		<p><?php _e( 'This plugin has cost me countless hours of work, if you use it, please donate a token of your appreciation!', 'pm4wp' ); ?></p>
-
-		<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-			<input type="hidden" name="cmd" value="_donations">
-			<input type="hidden" name="business" value="rilwis@gmail.com"> <input type="hidden" name="lc" value="US">
-			<input type="hidden" name="item_name" value="Private Messages For WordPress">
-			<input type="hidden" name="item_number" value="pm4wp">
-			<input type="hidden" name="currency_code" value="USD">
-			<input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_LG.gif:NonHosted">
-			<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-			<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-		</form>
-	</div>
-</div>
-			<?php
-
-}
-
-/**
  * Send form page
  */
-function rwpm_send( ) {
+function rwpm_send() {
 	global $wpdb, $current_user;
 	?>
 <div class="wrap">
@@ -299,7 +99,7 @@ function rwpm_send( ) {
 	$option = get_option( 'rwpm_option' );
 	if ( $_REQUEST['page'] == 'rwpm_send' && isset( $_POST['submit'] ) ) {
 		$error = false;
-		$status = array( );
+		$status = array();
 
 		// check if total pm of current user exceed limit
 		$role = $current_user->roles[0];
@@ -315,7 +115,7 @@ function rwpm_send( ) {
 		$content = strip_tags( $_POST['content'] );
 		$recipient = $option['type'] == 'autosuggest' ? explode( ',', $_POST['recipient'] ) : $_POST['recipient'];
 		$recipient = array_map( 'strip_tags', $recipient );
-		if ( get_magic_quotes_gpc( ) ) {
+		if ( get_magic_quotes_gpc() ) {
 			$subject = stripslashes( $subject );
 			$content = stripslashes( $content );
 			$recipient = array_map( 'stripslashes', $recipient );
@@ -401,7 +201,7 @@ function rwpm_send( ) {
 							$email_address = get_bloginfo( 'admin_email' );
 
 						$email_subject = strip_tags( $email_subject );
-						if ( get_magic_quotes_gpc( ) ) {
+						if ( get_magic_quotes_gpc() ) {
 							$email_subject = stripslashes( $email_subject );
 							$email_body = stripslashes( $email_body );
 						}
@@ -441,10 +241,10 @@ function rwpm_send( ) {
 			? $_GET['recipient'] : '' );
 
 		// strip slashes if needed
-		$subject = isset( $_REQUEST['subject'] ) ? ( get_magic_quotes_gpc( ) ? stripcslashes( $_REQUEST['subject'] )
+		$subject = isset( $_REQUEST['subject'] ) ? ( get_magic_quotes_gpc() ? stripcslashes( $_REQUEST['subject'] )
 			: $_REQUEST['subject'] ) : '';
 		$subject = urldecode( $subject );  // for some chars like '?' when reply
-		$content = isset( $_REQUEST['content'] ) ? ( get_magic_quotes_gpc( ) ? stripcslashes( $_REQUEST['content'] )
+		$content = isset( $_REQUEST['content'] ) ? ( get_magic_quotes_gpc() ? stripcslashes( $_REQUEST['content'] )
 			: $_REQUEST['content'] ) : '';
 
 		// Get all users of blog
@@ -456,7 +256,7 @@ function rwpm_send( ) {
 			<span id="all-users" style="display:none">
 					<script type="text/javascript">
 							<?php
-	   					$all = array( );
+	   					$all = array();
 							foreach ( $users as $user ) {
 								$user_data = array( 'value' => $user->display_name );
 								$all[] = $user_data;
@@ -506,7 +306,7 @@ function rwpm_send( ) {
 /**
  * Inbox page
  */
-function rwpm_inbox( ) {
+function rwpm_inbox() {
 	global $wpdb, $current_user;
 
 	// if view message
@@ -724,7 +524,7 @@ function rwpm_inbox( ) {
 /**
  * Outbox page
  */
-function rwpm_outbox( ) {
+function rwpm_outbox() {
 	global $wpdb, $current_user;
 
 	// if view message
@@ -890,7 +690,7 @@ function rwpm_outbox( ) {
  */
 add_action( 'admin_notices', 'rwpm_notify' );
 
-function rwpm_notify( ) {
+function rwpm_notify() {
 	global $wpdb, $current_user;
 
 	// get number of unread messages
