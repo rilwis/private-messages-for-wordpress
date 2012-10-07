466009370e6129cf108c8c5a5afbef8bc0093608
Anh Tran <rilwis@gmail.com>
2012-10-07 14:36:12 +0700

    Small cleanup

--- js/script.js
+++ js/script.js
@@ -1,37 +1,38 @@
-function split( val ) {
-    return val.split( /,\s*/ );
-}
-function extractLast( term ) {
-    return split( term ).pop();
-}
-jQuery(document).ready(function($) {
-    $('#recipient')
-    .autocomplete({
-        minLength: 2,
-        delay: 600,
-        source: function(request, response) {
-            var	data = {
-                action  : 'pm4wp_get_users',
-                term    : request.term
-            };
-            $.post(ajaxurl, data, function(r) {
-                response(r);
-            }, 'json');
-        },
-        focus: function() {
-            // prevent value inserted on focus
-            return false;
-        },
-        select: function( event, ui ) {
-            var terms = split( this.value );
-            // remove the current input
-            terms.pop();
-            // add the selected item
-            terms.push( ui.item.value );
-            // add placeholder to get the comma-and-space at the end
-            terms.push( "" );
-            this.value = terms.join( "," );
-            return false;
-        }
-    });
-});
+jQuery( document ).ready( function ( $ )
+{
+	$( '#recipient' ).autocomplete( {
+		source: function ( request, response )
+		{
+			var data = {
+				action: 'pm4wp_get_users',
+				term  : request.term
+			};
+			$.post( ajaxurl, data, function ( r )
+			{
+				response( r );
+			}, 'json' );
+		},
+		focus : function ()
+		{
+			// prevent value inserted on focus
+			return false;
+		},
+		select: function ( event, ui )
+		{
+			var terms = split( this.value );
+			// remove the current input
+			terms.pop();
+			// add the selected item
+			terms.push( ui.item.value );
+			// add placeholder to get the comma-and-space at the end
+			terms.push( "" );
+			this.value = terms.join( "," );
+			return false;
+		}
+	} );
+
+	function split( val )
+	{
+		return val.split( /,\s*/ );
+	}
+} );
--- pm4wp.php
+++ pm4wp.php
@@ -10,25 +10,30 @@ License: GNU GPL 2+
 */
 
 // Prevent loading this file directly
-if (!class_exists('WP')) {
-    header('Status: 403 Forbidden');
-    header('HTTP/1.1 403 Forbidden');
-    exit;
+defined( 'ABSPATH' ) || exit;
+
+define( 'PM4WP_DIR', plugin_dir_path( __FILE__ ) );
+define( 'PM4WP_INC_DIR', trailingslashit( PM4WP_DIR . 'inc/' ) );
+
+define( 'PM4WP_URL', plugin_dir_url( __FILE__ ) );
+define( 'PM4WP_CSS_URL', trailingslashit( PM4WP_URL . 'css' ) );
+define( 'PM4WP_JS_URL', trailingslashit( PM4WP_URL . 'js' ) );
+
+include_once PM4WP_INC_DIR . 'widget.php';
+include_once PM4WP_INC_DIR . 'inbox-page.php';
+include_once PM4WP_INC_DIR . 'send-page.php';
+include_once PM4WP_INC_DIR . 'outbox-page.php';
+
+if ( is_admin() )
+{
+	include_once PM4WP_INC_DIR . 'options.php';
 }
 
-define('PM4WP_DIR', plugin_dir_path(__FILE__));
-define('PM4WP_INC_DIR', trailingslashit(PM4WP_DIR . 'inc/'));
-
-define('PM4WP_URL', plugin_dir_url(__FILE__));
-define('PM4WP_CSS_URL', trailingslashit(PM4WP_URL . 'css'));
-define('PM4WP_JS_URL', trailingslashit(PM4WP_URL . 'js'));
-
-if (is_admin()) {
-    include_once PM4WP_INC_DIR . 'options.php';
-}
-
-add_action('plugins_loaded', 'rwpm_load_text_domain');
-register_activation_hook(__FILE__, 'rwpm_activate');
+register_activation_hook( __FILE__, 'rwpm_activate' );
+add_action( 'plugins_loaded', 'rwpm_load_text_domain' );
+add_action( 'admin_notices', 'rwpm_notify' );
+add_action( 'admin_bar_menu', 'rwpm_adminbar', 300 );
+add_action( 'wp_ajax_pm4wp_get_users', 'pm4wp_get_users' );
 
 /**
  * Load plugin text domain
@@ -37,7 +42,7 @@ register_activation_hook(__FILE__, 'rwpm_activate');
  */
 function rwpm_load_text_domain()
 {
-    load_plugin_textdomain('pm4wp', trailingslashit(PM4WP_DIR . 'lang'));
+	load_plugin_textdomain( 'pm4wp', false, trailingslashit( PM4WP_DIR . 'lang' ) );
 }
 
 /**
@@ -47,10 +52,10 @@ function rwpm_load_text_domain()
  */
 function rwpm_activate()
 {
-    global $wpdb;
+	global $wpdb;
 
-    // Create table
-    $query = 'CREATE TABLE IF NOT EXISTS ' . $wpdb->prefix . 'pm (
+	// Create table
+	$query = 'CREATE TABLE IF NOT EXISTS ' . $wpdb->prefix . 'pm (
 		`id` bigint(20) NOT NULL auto_increment,
 		`subject` text NOT NULL,
 		`content` text NOT NULL,
@@ -62,85 +67,78 @@ function rwpm_activate()
 		PRIMARY KEY (`id`)
 	) COLLATE utf8_general_ci;';
 
-    // Note: deleted = 1 if message is deleted by sender, = 2 if it is deleted by recipient
+	// Note: deleted = 1 if message is deleted by sender, = 2 if it is deleted by recipient
 
-    $wpdb->query($query);
+	$wpdb->query( $query );
 
-    // Default numbers of PM for each group
-    $default_option = array(
-        'administrator' => 0,
-        'editor' => 50,
-        'author' => 20,
-        'contributor' => 10,
-        'subscriber' => 5,
-        'type' => 'dropdown', // How to choose recipient: dropdown list or autocomplete based on user input
-        'email_enable' => 1,
-        'email_name' => '%BLOG_NAME%',
-        'email_address' => '%BLOG_ADDRESS%',
-        'email_subject' => __('New PM at %BLOG_NAME%', 'pm4wp'),
-        'email_body' => __("You have new private message from <b>%SENDER%</b> at <b>%BLOG_NAME%</b>.\n\n<a href=\"%INBOX_URL%\">Click here</a> to go to your inbox.\n\nThis email is sent automatically. Please don't reply.", 'pm4wp')
-    );
-    add_option('rwpm_option', $default_option, '', 'no');
+	// Default numbers of PM for each group
+	$default_option = array(
+		'administrator' => 0,
+		'editor'        => 50,
+		'author'        => 20,
+		'contributor'   => 10,
+		'subscriber'    => 5,
+		'type'          => 'dropdown', // How to choose recipient: dropdown list or autocomplete based on user input
+		'email_enable'  => 1,
+		'email_name'    => '%BLOG_NAME%',
+		'email_address' => '%BLOG_ADDRESS%',
+		'email_subject' => __( 'New PM at %BLOG_NAME%', 'pm4wp' ),
+		'email_body'    => __( "You have new private message from <b>%SENDER%</b> at <b>%BLOG_NAME%</b>.\n\n<a href=\"%INBOX_URL%\">Click here</a> to go to your inbox.\n\nThis email is sent automatically. Please don't reply.", 'pm4wp' )
+	);
+	add_option( 'rwpm_option', $default_option, '', 'no' );
 }
-
 
 /**
  * Show notification of new PM
  */
-add_action('admin_notices', 'rwpm_notify');
-
 function rwpm_notify()
 {
-    global $wpdb, $current_user;
+	global $wpdb, $current_user;
 
-    // get number of unread messages
-    $num_unread = $wpdb->get_var('SELECT COUNT(*) FROM ' . $wpdb->prefix . 'pm WHERE `recipient` = "' . $current_user->user_login . '" AND `read` = 0 AND `deleted` != "2"');
+	// get number of unread messages
+	$num_unread = $wpdb->get_var( 'SELECT COUNT(*) FROM ' . $wpdb->prefix . 'pm WHERE `recipient` = "' . $current_user->user_login . '" AND `read` = 0 AND `deleted` != "2"' );
 
-    if (empty($num_unread)) {
-        $num_unread = 0;
-    }
+	if ( empty( $num_unread ) )
+	{
+		$num_unread = 0;
+	}
 
-    if ($num_unread) {
-        echo '<div id="message" class="error"><p><b>', sprintf(_n('You have %d new message!', 'You have %d new messages!', $num_unread, 'pm4wp'), $num_unread), '</b> <a href="admin.php?page=rwpm_inbox">', __('Click here to go to inbox', 'pm4wp'), ' &raquo;</a></p></div>';
-    }
+	if ( $num_unread )
+	{
+		echo '<div id="message" class="error"><p><b>', sprintf( _n( 'You have %d new message!', 'You have %d new messages!', $num_unread, 'pm4wp' ), $num_unread ), '</b> <a href="admin.php?page=rwpm_inbox">', __( 'Click here to go to inbox', 'pm4wp' ), ' &raquo;</a></p></div>';
+	}
 }
 
 function rwpm_adminbar()
 {
-    global $wp_admin_bar;
-    global $wpdb, $current_user;
+	global $wp_admin_bar;
+	global $wpdb, $current_user;
 
-    // get number of unread messages
-    $num_unread = $wpdb->get_var('SELECT COUNT(*) FROM ' . $wpdb->prefix . 'pm WHERE `recipient` = "' . $current_user->user_login . '" AND `read` = 0 AND `deleted` != "2"');
+	// get number of unread messages
+	$num_unread = $wpdb->get_var( 'SELECT COUNT(*) FROM ' . $wpdb->prefix . 'pm WHERE `recipient` = "' . $current_user->user_login . '" AND `read` = 0 AND `deleted` != "2"' );
 
-    if (empty($num_unread)) {
-        $num_unread = 0;
-    }
+	if ( empty( $num_unread ) )
+	{
+		$num_unread = 0;
+	}
 
-    if ($num_unread && is_admin_bar_showing()) {
-        $wp_admin_bar->add_menu(array(
-            'id' => 'rwpm',
-            'title' => sprintf(_n('You have %d new message!', 'You have %d new messages!', $num_unread, 'pm4wp'), $num_unread),
-            'href' => admin_url('admin.php?page=rwpm_inbox'),
-            'meta' => array('class' => "rwpm_newmessages"),
-        ));
-    }
+	if ( $num_unread && is_admin_bar_showing() )
+	{
+		$wp_admin_bar->add_menu( array(
+			'id'    => 'rwpm',
+			'title' => sprintf( _n( 'You have %d new message!', 'You have %d new messages!', $num_unread, 'pm4wp' ), $num_unread ),
+			'href'  => admin_url( 'admin.php?page=rwpm_inbox' ),
+			'meta'  => array( 'class' => "rwpm_newmessages" ),
+		) );
+	}
 }
 
-add_action( 'wp_ajax_pm4wp_get_users', 'pm4wp_get_users' );
 function pm4wp_get_users()
 {
-    global $wpdb;
+	global $wpdb;
 
-    $keyword = trim( strip_tags( $_POST['keyword'] ) );
-    $values = $wpdb->get_col( "SELECT DISTINCT display_name FROM {$wpdb->users} WHERE display_name LIKE '{$keyword}%'" );
+	$keyword = trim( strip_tags( $_POST['term'] ) );
+	$values = $wpdb->get_col( "SELECT DISTINCT display_name FROM {$wpdb->users} WHERE display_name LIKE '{$keyword}%'" );
 
-die( json_encode( $values ) );
+	die( json_encode( $values ) );
 }
-
-add_action('admin_bar_menu', "rwpm_adminbar", 300);
-
-include_once PM4WP_INC_DIR . 'widget.php';
-include_once PM4WP_INC_DIR . 'inbox-page.php';
-include_once PM4WP_INC_DIR . 'send-page.php';
-include_once PM4WP_INC_DIR . 'outbox-page.php';
