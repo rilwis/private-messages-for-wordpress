b6248333fd287987599061df29a17145d86647e4
Anh Tran <rilwis@gmail.com>
2012-11-28 11:28:34 +0700

    Fix bug: send to multiple recipients

--- js/script.js
+++ js/script.js
@@ -1,45 +1,49 @@
 jQuery( document ).ready( function ( $ )
 {
+
+	/**
+	 * Split string into multiple values, separated by commas
+	 *
+	 * @param val
+	 *
+	 * @return array
+	 */
+	function split( val )
+	{
+		return val.split( /,\s*/ );
+	}
+
+	/**
+	 * Extract string Last into multiple values
+	 * @param term
+	 *
+	 */
+	function extract_last( term )
+	{
+		return split( term ).pop();
+	}
+
 	$( '#recipient' ).autocomplete( {
 		source: function ( request, response )
 		{
 			var data = {
 				action: 'rwpm_get_users',
-				term  : request.term
+				term  : extract_last( request.term )
 			};
 			$.post( ajaxurl, data, function ( r )
 			{
 				response( r );
 			}, 'json' );
 		},
-		focus : function ()
-		{
-			// prevent value inserted on focus
-			return false;
-		},
 		select: function ( event, ui )
 		{
 			var terms = split( this.value );
-			// remove the current input
 			terms.pop();
-			// add the selected item
 			terms.push( ui.item.value );
-			// add placeholder to get the comma-and-space at the end
 			terms.push( "" );
 			this.value = terms.join( "," );
 			return false;
 		}
 	} );
 
-    /**
-     * Split string into multiple values, separated by commas
-     *
-     * @param val
-     *
-     * @return array
-     */
-	function split( val )
-	{
-		return val.split( /,\s*/ );
-	}
 } );
